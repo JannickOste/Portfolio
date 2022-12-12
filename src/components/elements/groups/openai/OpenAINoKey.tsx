ad7e0b659557d85304436b59f80b12658f3035ac
Oste Jannick <oste.jannick@hotmail.com>
2022-12-12 16:51:08 +0100

    English to dutch typo fix

--- src/components/elements/groups/openai/OpenAINoKey.tsx
+++ src/components/elements/groups/openai/OpenAINoKey.tsx
@@ -29,10 +29,24 @@ export default class OpenAINoKey extends React.Component<ChatNoKeyFormProps, Cha
     public render = (): React.ReactNode => {
         return (<>
             <div className="bg-danger p-2 text-white text-center mb-2">
-                No OpenAI key found, disabled for live preview, optionaly provide your own key below with a max credit limit 
+                Geen OpenAI code gevonden. Maak een code aan en geef deze op of een huidige OpenAI API code.
             </div>
 
-            <form className="d-flex flex-column mt-5" onSubmit={this.onSubmit}>
+            <p className="mt-5 h5">Geen code? Geen probleem!</p>
+            <pre className="border rounded p-2" style={{whiteSpace: "pre-wrap"}}>
+                <div>
+                    <strong>Stap 1:</strong> Ga naar de website van <a href="https://beta.openai.com/signup" title="Registratie link">OpenAI</a> en klik op de optie om een account aan te maken. Je kunt kiezen om in te loggen via email, google of microsoft. Volg de stappen op het scherm om je account aan te maken.
+                </div>
+                <div>
+                    <strong>Stap 2:</strong> Nadat je een account hebt aangemaakt, ga je naar de pagina met <a href="https://beta.openai.com/account/api-keys" title="API sleutels verkrijgen">API-sleutels</a> op de website van OpenAI. Klik op de knop om een nieuwe sleutel te genereren en volg de instructies op het scherm om dit te doen.
+                </div>
+                <div>
+                    <strong>Stap 3:</strong> Nadat je een API-sleutel hebt gegenereerd, kun je deze gebruiken om toegang te krijgen tot de diensten van OpenAI. Voer de sleutel in op het webplatform waar je de diensten van OpenAI wilt gebruiken en volg de instructies op het scherm om de integratie te voltooien.
+                </div>
+            </pre>
+
+            <p className="h5 mt-5">API code instellen</p>
+            <form className="d-flex flex-column border rounded p-2" onSubmit={this.onSubmit}>
                 <div className="d-sm-flex">
                     <label htmlFor="key" style={{flex: 1}}>API Key:</label>
                     <input 
